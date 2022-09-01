99828eccfc0b37bc4e0a22ca5dbfebd36f88d290
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2022-09-01 13:11:39 +0800

    Implemented skeleton of Encoder class

--- Encoder.java
+++ Encoder.java
@@ -1,3 +1,19 @@
 public class Encoder implements EncoderInterface {
+    ReferenceTable refTable;
 
+    public Encoder(ReferenceTable refTable) {
+        this.refTable = refTable;
+    }
+
+    @Override
+    public String encode(String s, char offset) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String decode(String s) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
--- EncoderInterface.java
+++ EncoderInterface.java
@@ -1,3 +1,5 @@
 public interface EncoderInterface {
+    public String encode(String s, char offset);
 
+    public String decode(String s);
 }
